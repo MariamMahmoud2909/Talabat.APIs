b810d7d0a51bcb4d6b011dc0597023d43e6e120c
Mariam Mahmoud <105247169+MariamMahmoud2909@users.noreply.github.com>
2024-04-10 08:19:00 +0200

    Product Module - Entities

--- Talabat.Core/Entities/Product.cs
+++ Talabat.Core/Entities/Product.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace Talabat.Core.Entities
 {
-	internal class Product
+	public class Product : BaseEntity
 	{
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public string PictureUrl { get; set; }
+		public decimal Price { get; set; }
+		public int BrandId { get; set; } // Foreign Key 
+		public ProductBrand Brand { get; set; }
+		public int CategoryId { get; set; } // Foreign Key 
+		public ProductCategory Category { get; set; }
 	}
 }
